1c32dd3abd055fc321e07bf30d1cf00660a9fa7e
YashwanthK <yashwanthkothakota@gmail.com>
2023-10-06 22:24:02 +0530

    feature / add voting

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,7 +1,150 @@
+"use client";
+import { downvoteAnswer, upvoteAnswer } from "@/lib/actions/answer.action";
+import {
+  downvoteQuestion,
+  upvoteQuestion,
+} from "@/lib/actions/question.action";
+import { formatLargeNumber } from "@/lib/utils";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+
 import React from "react";
 
-const Votes = () => {
-  return <div>Votes</div>;
+interface Props {
+  type: string;
+  itemId: string;
+  userId: string;
+  upvotes: number;
+  hasUpVoted: boolean;
+  downvotes: number;
+  hasDownVoted: boolean;
+  hasSaved?: boolean;
+}
+
+const Votes = ({
+  type,
+  itemId,
+  userId,
+  upvotes,
+  hasUpVoted,
+  downvotes,
+  hasDownVoted,
+  hasSaved,
+}: Props) => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleSave = () => {};
+
+  const handleVote = async (action: string) => {
+    if (!userId) {
+      return;
+    }
+    if (action === "upvote") {
+      if (type === "Question") {
+        await upvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted: hasUpVoted,
+          hasdownVoted: hasDownVoted,
+          path: pathname,
+        });
+      } else if (type === "Answer") {
+        await upvoteAnswer({
+          answerId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted: hasUpVoted,
+          hasdownVoted: hasDownVoted,
+          path: pathname,
+        });
+      }
+
+      // todo:show a toast
+      return;
+    }
+
+    if (action === "downvote") {
+      if (type === "Question") {
+        await downvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted: hasUpVoted,
+          hasdownVoted: hasDownVoted,
+          path: pathname,
+        });
+      } else if (type === "Answer") {
+        await downvoteAnswer({
+          answerId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted: hasUpVoted,
+          hasdownVoted: hasDownVoted,
+          path: pathname,
+        });
+      }
+
+      // todo:show a toast
+    }
+  };
+
+  return (
+    <div className="flex gap-5">
+      <div className="flex-center gap-2.5">
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasUpVoted
+                ? "/assets/icons/upvoted.svg"
+                : "/assets/icons/upvote.svg"
+            }
+            alt="upvote"
+            width={18}
+            height={18}
+            className="cursor-pointer"
+            onClick={() => handleVote("upvote")}
+          />
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatLargeNumber(upvotes)}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasDownVoted
+                ? "/assets/icons/downvoted.svg"
+                : "/assets/icons/downvote.svg"
+            }
+            alt="downvote"
+            width={18}
+            height={18}
+            className="cursor-pointer"
+            onClick={() => handleVote("downvote")}
+          />
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatLargeNumber(downvotes)}
+            </p>
+          </div>
+        </div>
+      </div>
+      {type === "Question" && (
+        <Image
+          src={
+            hasSaved
+              ? "/assets/icons/star-filled.svg"
+              : "/assets/icons/star-red.svg"
+          }
+          alt="star"
+          width={18}
+          height={18}
+          className="cursor-pointer"
+          onClick={handleSave}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Votes;
