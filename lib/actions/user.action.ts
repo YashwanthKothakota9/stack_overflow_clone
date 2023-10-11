ea474f234da08095d4289461a5a90afd18927537
YashwanthK <yashwanthkothakota@gmail.com>
2023-10-11 10:17:12 +0530

    feature / add filters on community page

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -100,7 +100,7 @@ export async function getAllUsers(params: GetAllUsersParams) {
     connectToDatabase();
 
     // const { page = 1, pageSize = 20, filter, searchQuery } = params;
-    const { searchQuery } = params;
+    const { searchQuery, filter } = params;
     const query: FilterQuery<typeof User> = {};
     if (searchQuery) {
       query.$or = [
@@ -108,7 +108,25 @@ export async function getAllUsers(params: GetAllUsersParams) {
         { username: { $regex: new RegExp(searchQuery, "i") } },
       ];
     }
-    const users = await User.find({ query }).sort({ createdAt: -1 });
+
+    let sortOptions = {};
+
+    switch (filter) {
+      case "new_users":
+        sortOptions = { joinedAt: -1 };
+        break;
+      case "old_users":
+        sortOptions = { joinedAt: 1 };
+        break;
+      case "top_contributors":
+        sortOptions = { reputation: -1 };
+        break;
+
+      default:
+        break;
+    }
+
+    const users = await User.find({ query }).sort(sortOptions);
 
     return { users };
   } catch (error) {
